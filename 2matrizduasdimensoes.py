8a88c82c139be3f46bac1b695a306277e5659fa0
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-05-11 20:11:41 -0300

    Update 2matrizduasdimensoes.py

--- 2matrizduasdimensoes.py
+++ 2matrizduasdimensoes.py
@@ -1,4 +1,4 @@
 """
-2)	Ler uma matriz A de duas dimensões com 7 linhas e 7 colunas. Ao final apresentar o
+2) Ler uma matriz A de duas dimensões com 7 linhas e 7 colunas. Ao final apresentar o
 total de elementos pares existentes dentro da matriz.
 """
