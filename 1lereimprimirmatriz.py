bfbfe76e20612b580aabe7a532842d6877e4e385
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-05-18 13:12:56 -0300

    Update 1lereimprimirmatriz.py
    update of code

--- 1lereimprimirmatriz.py
+++ 1lereimprimirmatriz.py
@@ -16,3 +16,21 @@ for i in range(2):
     for j in range(4):
         print(matriz[i][j], end=" ")
     print()
+
+   
+#Codigo do professor:
+
+#int matriz[][] = new int[2][4]; JAVA
+#               COL              LIN
+import random
+matriz = [[0] * 4 for i in range(2)]
+
+for lin in range(2):
+    for col in range(4):
+        matriz[lin][col] = random.randint(0,100)
+#print(matriz)
+
+for lin in range(2):
+    for col in range(4):
+        print(matriz[lin][col], end=" ")
+    print()
